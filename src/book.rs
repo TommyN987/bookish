cd96fab48db428fdfe587f958272a387cc43c926
tommy <tomas.nagy.tn@gmail.com>
2023-05-31 01:12:56 +0200

    implement top_five and bottom_five

--- src/book.rs
+++ src/book.rs
@@ -4,26 +4,42 @@ use std::collections::HashMap;
 struct Book {
     title: String,
     content: String,
+    sorted_words: Vec<(String, i32)>,
 }
 
 impl Book {
     pub fn new(title: String, content: String) -> Self {
-        Self { title, content }
-    }
-
-    fn sort_words(&self) -> Vec<(String, i32)> {
-        let mut word_dictionary: HashMap<String, i32> = HashMap::new();
-        let uncommon_words: Vec<String> = filter_stop_words(&self.content);
-
-        for word in uncommon_words {
-            *word_dictionary.entry(word).or_insert(0) += 1;
+        Self {
+            title,
+            sorted_words: sort_words(&content),
+            content,
         }
-
-        let mut return_vec: Vec<_> = word_dictionary.into_iter().collect();
-        return_vec.sort_unstable_by(|a, b| b.1.cmp(&a.1));
-
-        return_vec
     }
+
+    pub fn top_five(&self) -> &[(String, i32)] {
+        let upper_bound = self.sorted_words.len().min(5);
+        &self.sorted_words[0..upper_bound]
+    }
+
+    pub fn bottom_five(&self) -> &[(String, i32)] {
+        let len = self.sorted_words.len();
+        let start = len.saturating_sub(5);
+        &self.sorted_words[start..]
+    }
+}
+
+fn sort_words(content: &String) -> Vec<(String, i32)> {
+    let mut word_dictionary: HashMap<String, i32> = HashMap::new();
+    let uncommon_words: Vec<String> = filter_stop_words(&content);
+
+    for word in uncommon_words {
+        *word_dictionary.entry(word).or_insert(0) += 1;
+    }
+
+    let mut return_vec: Vec<_> = word_dictionary.into_iter().collect();
+    return_vec.sort_unstable_by(|a, b| b.1.cmp(&a.1));
+
+    return_vec
 }
 
 #[cfg(test)]
@@ -32,23 +48,16 @@ mod tests {
 
     #[test]
     fn test_sort_words_single_word() {
-        let book = Book {
-            title: String::from("Test Book"),
-            content: String::from("hello hello hello hello hello world"),
-        };
-        let result = book.sort_words();
+        let content = String::from("hello hello hello hello hello world");
+        let result = sort_words(&content);
         let expected = vec![("hello".to_string(), 5), ("world".to_string(), 1)];
         assert_eq!(result, expected);
     }
 
     #[test]
     fn test_sort_words_multiple_words() {
-        let content = "Hello world! Love, love, world. World! Peace";
-        let book = Book {
-            title: "Test".to_string(),
-            content: content.to_string(),
-        };
-        let mut result = book.sort_words();
+        let content = String::from("Hello world! Love, love, world. World! Peace");
+        let mut result = sort_words(&content);
         let mut expected = vec![
             ("world".to_string(), 3),
             ("love".to_string(), 2),
@@ -58,5 +67,40 @@ mod tests {
         result.sort_by(|a, b| b.1.cmp(&a.1).then_with(|| a.0.cmp(&b.0)));
         expected.sort_by(|a, b| b.1.cmp(&a.1).then_with(|| a.0.cmp(&b.0)));
         assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_top_five() {
+        let book = Book::new(
+            "title".to_string(), 
+            "Lorem lorem lorem lorem lorem lorem lorem Ipsum Ipsum Ipsum Ipsum dummy, dummy. Hamilton, hamilton, hamilton, bla im alone hamilton, hamilton. Tommy tommy, tommy".to_string());
+        let result: &[(String, i32)] = book.top_five();
+
+        let expected: [(String, i32); 5] = [
+            ("lorem".to_string(), 7),
+            ("hamilton".to_string(), 5),
+            ("ipsum".to_string(), 4),
+            ("tommy".to_string(), 3),
+            ("dummy".to_string(), 2),
+        ];
+        assert_eq!(result, &expected[..]);
+    }
+
+    #[test]
+    fn test_bottom_five() {
+        let book = Book::new(
+            "title".to_string(), 
+            "Lorem lorem lorem lorem lorem lorem lorem Ipsum ipsum ipsum Ipsum Ipsum blah tommy tommy Ipsum dummy bla bla, dummy dummy. Hamilton dummy Hamilton, hamilton, hamilton, bla blah alone hamilton, hamilton. Tommy tommy, tommy".to_string());
+        let result = book.bottom_five();
+
+        let expected: [(String, i32); 5] = [
+            ("tommy".to_string(), 5),
+            ("dummy".to_string(), 4),
+            ("bla".to_string(), 3),
+            ("blah".to_string(), 2),
+            ("alone".to_string(), 1),
+        ];
+
+        assert_eq!(result, expected);
     }
 }
